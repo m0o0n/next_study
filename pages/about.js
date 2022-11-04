7ad31ac59c595bf0fd7b37f8a6ed286b5f740537
m0o0n <strikunvlad@gmail.com>
2022-11-04 17:47:10 +0200

    delete unused languages

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,7 @@ import useTranslation from 'next-translate/useTranslation';
 import { MainLayout } from '../components/MainLayout';
 export default function About() {
 	const { t } = useTranslation('common');
-	console.log(useTranslation('common'));
+
 	return (
 		<MainLayout>
 			<h1>{t('about')}</h1>
